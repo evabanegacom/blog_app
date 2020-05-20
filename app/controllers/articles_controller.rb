e3b4b95f48cc49b1a63e4d9b7577284234e18545
Precious chiedu <udegbue69@gmail.com>
2020-05-20 12:43:43 +0100

    created actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,21 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    #render json: params
+  end
+
+  def create
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.save
+    redirect_to article_path(@article)
+  end
 end
